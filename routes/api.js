028c688f59dba999dc4abc605526da654d64c43c
James Perih <james@hotdang.ca>
2020-05-10 11:15:03 -0600

    leaderboard data endpoint.

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,342 @@ var bcrypt = require('bcryptjs');
 
 // var PUBLIC_URL = 'http://rbyc.hotdang.ca';
 
+// TODO: replace with actual data calls
+const _leaderboardData = {
+  divisions: [
+      {
+          name: 'division a',
+          events: [
+              {                  
+                  name: 'event a',
+                  scores: [
+                      {
+                          scoreId: 1,
+                          place: 1,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'Crossfit Truro',
+                          gymName: 'Crossfit Truro',
+                          score: 261,
+                      },
+                      {
+                          scoreId: 2,
+                          place: 2,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'CFA Misfits',
+                          gymName: 'CROSSFIT ASCOT',
+                          score: 260,
+                      },
+                      {
+                          scoreId: 3,
+                          place: 3,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'THE BOX ROGUES',
+                          gymName: 'THE BOX - CROSSFIT TAUNTON',
+                          score: 259,
+                      },
+                      {
+                          scoreId: 4,
+                          place: 1,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'Crossfit Truro',
+                          gymName: 'Crossfit Truro',
+                          score: 261,
+                      },
+                      {
+                          scoreId: 5,
+                          place: 2,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'CFA Misfits',
+                          gymName: 'CROSSFIT ASCOT',
+                          score: 260,
+                      },
+                      {
+                          scoreId: 6,
+                          place: 3,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'THE BOX ROGUES',
+                          gymName: 'THE BOX - CROSSFIT TAUNTON',
+                          score: 259,
+                      },
+                      {
+                          scoreId: 7,
+                          place: 4,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'Crossfit Truro',
+                          gymName: 'Crossfit Truro',
+                          score: 261,
+                      },
+                      {
+                          scoreId: 8,
+                          place: 5,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'CFA Misfits',
+                          gymName: 'CROSSFIT ASCOT',
+                          score: 260,
+                      },
+                      {
+                          scoreId: 9,
+                          place: 6,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'THE BOX ROGUES',
+                          gymName: 'THE BOX - CROSSFIT TAUNTON',
+                          score: 259,
+                      },
+                      {
+                          scoreId: 10,
+                          place: 4,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'Crossfit Truro',
+                          gymName: 'Crossfit Truro',
+                          score: 261,
+                      },
+                      {
+                          scoreId: 11,
+                          place: 5,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'CFA Misfits',
+                          gymName: 'CROSSFIT ASCOT',
+                          score: 260,
+                      },
+                      {
+                          scoreId: 12,
+                          place: 6,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'THE BOX ROGUES',
+                          gymName: 'THE BOX - CROSSFIT TAUNTON',
+                          score: 259,
+                      },
+                  ],
+              },
+              {                  
+                  name: 'event b',
+                  scores: [
+                      {
+                          scoreId: 13,
+                          place: 1,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'Crossfit Truro',
+                          gymName: 'Crossfit Truro',
+                          score: 400,
+                      },
+                      {
+                          scoreId: 14,
+                          place: 2,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'CFA Misfits',
+                          gymName: 'CROSSFIT ASCOT',
+                          score: 392,
+                      },
+                      {
+                          scoreId: 15,
+                          place: 3,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'THE BOX ROGUES',
+                          gymName: 'THE BOX - CROSSFIT TAUNTON',
+                          score: 199,
+                      },
+                      {
+                          scoreId: 16,
+                          place: 1,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'Crossfit Truro',
+                          gymName: 'Crossfit Truro',
+                          score: 500,
+                      },
+                      {
+                          scoreId: 17,
+                          place: 2,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'CFA Misfits',
+                          gymName: 'CROSSFIT ASCOT',
+                          score: 480,
+                      },
+                      {
+                          scoreId: 18,
+                          place: 3,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'THE BOX ROGUES',
+                          gymName: 'THE BOX - CROSSFIT TAUNTON',
+                          score: 450,
+                      }
+                  ],
+              }
+          ]
+      },
+      {
+          name: 'division b',
+          events: [
+              {                  
+                  name: 'event c',
+                  scores: [
+                      {
+                          scoreId: 19,
+                          place: 3,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'Crossfit Truro',
+                          gymName: 'Crossfit Truro',
+                          score: 100,
+                      },
+                      {
+                          scoreId: 20,
+                          place: 2,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'CFA Misfits',
+                          gymName: 'CROSSFIT ASCOT',
+                          score: 75,
+                      },
+                      {
+                          scoreId: 21,
+                          place: 1,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'THE BOX ROGUES',
+                          gymName: 'THE BOX - CROSSFIT TAUNTON',
+                          score: 50,
+                      },
+                      {
+                          scoreId: 22,
+                          place: 1,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'Crossfit Truro',
+                          gymName: 'Crossfit Truro',
+                          score: 100,
+                      },
+                      {
+                          scoreId: 23,
+                          place: 3,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'CFA Misfits',
+                          gymName: 'CROSSFIT ASCOT',
+                          score: 75,
+                      },
+                      {
+                          scoreId: 24,
+                          place: 2,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'THE BOX ROGUES',
+                          gymName: 'THE BOX - CROSSFIT TAUNTON',
+                          score: 100,
+                      }
+                  ],
+              },
+              {                  
+                  name: 'event d',
+                  scores: [
+                      {
+                          scoreId: 25,
+                          place: 1,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'Crossfit Truro',
+                          gymName: 'Crossfit Truro',
+                          score: 175,
+                      },
+                      {
+                          scoreId: 26,
+                          place: 2,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'CFA Misfits',
+                          gymName: 'CROSSFIT ASCOT',
+                          score: 150,
+                      },
+                      {
+                          scoreId: 27,
+                          place: 3,
+                          firstName: 'James',
+                          lastInitial: 'P',
+                          gender: 'M',
+                          teamName: 'THE BOX ROGUES',
+                          gymName: 'THE BOX - CROSSFIT TAUNTON',
+                          score: 100,
+                      },
+                      {
+                          scoreId: 28,
+                          place: 1,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'Crossfit Truro',
+                          gymName: 'Crossfit Truro',
+                          score: 175,
+                      },
+                      {
+                          scoreId: 29,
+                          place: 2,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'CFA Misfits',
+                          gymName: 'CROSSFIT ASCOT',
+                          score: 150,
+                      },
+                      {
+                          scoreId: 30,
+                          place: 3,
+                          firstName: 'Dara',
+                          lastInitial: 'P',
+                          gender: 'F',
+                          teamName: 'THE BOX ROGUES',
+                          gymName: 'THE BOX - CROSSFIT TAUNTON',
+                          score: 100,
+                      }
+                  ],
+              }
+          ]
+      },
+  ],
+};
+
 var dbController = require('../models');
 const ROLES = {
   User: 'user',
@@ -92,142 +428,13 @@ router.get('/users/gym/:gymName', (req, res, next) => {
   });
 });
 
-
 /**
- * List Customers
+ * Leaderboard data stub
  */
-router.get('/customers', (req, res, next) => {
-  // if (!checkToken(req.jwt)) {
-  //   return res.status(403).json({ error: 'Unauthenticated' });
-  // }; // TODO: this should be middleware
-
-  dbController.Customers.list({}, null, (customers) => {
-    if (customers) {
-      return res.status(200).json({
-        customers,
-      });
-    }
-  });
+router.get('/leaderboards/all', (req, res, next) => {
+  const payload = _leaderboardData;
+  res.status(200).json(payload);
 });
 
-/**
- * Get user by Id
- */
-router.get('/customers/:customerId', (req, res, next) => {
-
-});
-
-/**
- * Add Customer
- */
-router.post('/customers', (req, res, next) => {
-  // if (!checkToken(req.jwt)) {
-  //   return res.status(403).json({ error: 'Unauthenticated' });
-  // }; // TODO: this should be middleware
-
-  // const { sub, role: adminRole } = req.jwt.payload;
-  // if (adminRole !== ROLES.Admin) {
-  //   return res.status(401).json({ error: 'Insufficient privileges to do that.'});
-  // }
-
-  const {
-    memberClass,
-    name,
-    contactSource,
-    street,
-    city,
-    province,
-    postalCode,
-    phone,
-    email,
-    secondaryEmail,
-  } = req.body;
-
-  // Validation
-  // if (!lastName || !location || !percentage ) { // firstName can be optional, email and password optional
-  //   return res.status(400).json({ error: 'Not enough params' });
-  // }
-
-  const newCustomer = {
-    memberClass,
-    name,
-    contactSource,
-    street,
-    city,
-    province,
-    postalCode,
-    phone,
-    email,
-    secondaryEmail,
-  };
-
-  // TODO: find this customer, if firstname and lastname show up, reject.
-  // const dupUser = DOCTORS.filter((user) => {
-  //   return user.firstName === firstName && user.lastName === lastName;
-  // })[0];
-  // if (dupUser) {
-  //   return res.status(409).json({ error: 'Already have that physician' });
-  // }
-
-  dbController.Customers.add(newCustomer, (created) => {
-    return res.status(201).json({
-      success: 'true',
-      customer: created,
-    });
-  })
-});
-
-/**
- * Edit Doctor
- */
-router.patch('/customers/:customerId', (req, res, next) => {
-  // if (!checkToken(req.jwt)) {
-  //   return res.status(403).json({ error: 'Unauthenticated' });
-  // }; // TODO: this should be middleware
-
-  // const { sub, role } = req.jwt.payload;
-  // if (role !== ROLES.Admin) {
-  //   return res.status(401).json({ error: 'Insufficient privileges to do that.'});
-  // }
-
-  const customer = req.body;
-  // if (doctorObject.password) {
-  //   doctorObject.password = bcrypt.hashSync(doctorObject.password);
-  // }
-
-  dbController.Customers.update(req.params.customerId, customer, (updated) => {
-    return res.status(201).json({
-      success: true,
-      customer: updated,
-    });
-  });
-});
-
-/**
- * Delete Doctor
- */
-router.delete('/customers/:customerId', (req, res, next) => {
-  // if (!checkToken(req.jwt)) {
-  //   return res.status(403).json({ error: 'Unauthenticated' });
-  // }; // TODO: this should be middleware
-
-  // const { sub, role } = req.jwt.payload;
-  // if (role !== ROLES.Admin) {
-  //   return res.status(401).json({ error: 'Insufficient privileges to do that.'});
-  // }
-
-
-  // TODO: what about the doctors who are at this location?!?
-  dbController.Customers.delete({ _id: req.params.customerId }, (result) => {
-    let message;
-    if (result && result._id) {
-      message = 'Deleted Customer';
-    } else {
-      message = 'Could not delete customer.';
-    }
-
-    return res.status(result && result._id ? 201 : 404).json({ message });
-  });
-});
 
 module.exports = router;
