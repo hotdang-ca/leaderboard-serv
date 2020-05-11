1bf4c56ddee27c9a81a7fc64e4b223b352e9344c
James Perih <james@hotdang.ca>
2020-05-11 10:31:03 -0600

    Sanitize db. oops.

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
 //TODO: everything!
 const mongoose = require('mongoose');
 
-const DB_USER = process.env.DB_USER || 'villain';
-const DB_PASS = process.env.DB_PASS || 'Vumbec-nexqoz-0pakji';
-const DB_SERVER = process.env.DB_SERVER || 'ds011810.mlab.com';
-const DB_PORT = process.env.DB_PORT || 11810;
-const DB_ENDPOINT = process.env.DB_ENDPOINT || 'villainstrength';
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASSl
+const DB_SERVER = process.env.DB_SERVER;
+const DB_PORT = process.env.DB_PORT;
+const DB_ENDPOINT = process.env.DB_ENDPOINT;
 
 const CONN = `mongodb://${DB_USER}:${DB_PASS}@${DB_SERVER}:${DB_PORT}/${DB_ENDPOINT}`;
 mongoose.Promise = global.Promise;
